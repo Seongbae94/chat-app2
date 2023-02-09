a6e567ed906cbf9d68616562880bc881189c6e41
Seongbae94 <seongbaelee944@gmail.com>
2023-02-09 14:36:33 +0900

    feat: 로그인 페이지 뼈대 작성
    로그인 페이지 뼈대 / 스타일 작성

--- chat-app/src/pages/Login.tsx
+++ chat-app/src/pages/Login.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">채팅하자</span>
+        <span className="title">회원가입</span>
+        <form>
+          <input type="email" placeholder="이메일" />
+          <input type="password" placeholder="비밀번호" />
+
+          <button>로그인</button>
+          {/* {err && <span>문제가 발생하였습니다</span>} */}
+        </form>
+        <p>계정이 없으세요? 회원가입하기</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
